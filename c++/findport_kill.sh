0dd8084ca6ba5c0d8da31768b88ea5d5ad05b4b4
nianer <qdlxgp@163.com>
2021-11-05 15:18:04 +0800

    Add files via upload

--- c++/findport_kill.sh
+++ c++/findport_kill.sh
@@ -6,11 +6,16 @@ else
         sed 's/ //g' 1 > 2
         echo | grep -o LISTEN.* 2 > 3
         v1=$(cat 3)
-        v2=`echo $v1 | tr -cd "[0-9]"`
-        if [ ! $v2 ] ; then
+        #v2=`echo $v1 | tr -cd "[0-9]"`
+        echo v1=$v1
+        v2=$(echo ${v1#LISTEN})
+        v3=$(echo ${v2%/.*})
+        echo v2=$v2
+        echo v3=$v3
+        if [ ! $v3 ] ; then
                 echo "No process is occupying the port !!" 
         else 
-                kill -9 $v2
+                kill -9 $v3
                 echo "kill success"
         fi
         rm -f 1
